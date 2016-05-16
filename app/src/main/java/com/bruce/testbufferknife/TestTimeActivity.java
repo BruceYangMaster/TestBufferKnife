0249df2f7d139cefdb3ab97e0bfe4a751932cf32
bruceyangjie <18328434957@163.com>
2016-05-16 21:34:58 +0800

    改了一点

--- app/src/main/java/com/bruce/testbufferknife/TestTimeActivity.java
+++ app/src/main/java/com/bruce/testbufferknife/TestTimeActivity.java
@@ -21,7 +21,6 @@ public class TestTimeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         initDialog();
-
     }
 
     private void initDialog() {
